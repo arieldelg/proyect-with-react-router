04b7042349f94cca4b3073970fd2233726e1ad7b
arieldelg <136664352+arieldelg@users.noreply.github.com>
2024-01-10 19:52:55 -0700

    update name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://arieldelg.github.io/react-router-dom-V6.4/'
+  base: 'https://arieldelg.github.io/proyect-with-react-router/'
 })
